1a60a1a3eeff5f92236df74cbb8c077c494f7095
medihermanto <meditinambunan27@gmail.com>
2022-07-03 16:05:17 +0800

    setting routes midtrans

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-
+// midtrans routes
+Route::get('payment/success', [UserController::class, 'midtransCallback']);
+Route::post('payment/success', [UserController::class, 'midtransCallback']);
 
 Route::middleware(['auth'])->group(function() {
     // checkout routes
